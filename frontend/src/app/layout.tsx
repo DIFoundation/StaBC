7e05bf18c7ce6352b2484ce01a0665e4fecf7c91
DIFoundation <adeniranibrahim165@gmail.com>
2025-10-26 12:06:18 +0100

    appkit working

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+
+import { headers } from 'next/headers' // added
+import ContextProvider from '@/context'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,21 +15,25 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const inter = Inter({ subsets: ['latin'] })
+
 export const metadata: Metadata = {
   title: "Staking App",
   description: "A decentralized staking application",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headersObj = await headers();
+  const cookies = headersObj.get('cookie')
+
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
-          {children}
-
+      <body className={`${geistSans.variable} ${geistMono.variable} ${inter.style} font-sans`}>
+        <ContextProvider cookies={cookies}>{children}</ContextProvider>
       </body>
     </html>
   );
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <h1>Staking App</h1>
+        <appkit-account-button />
+    </div>
+  )
+}
+
+export default Header
